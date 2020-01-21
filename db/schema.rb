a1a0fdab330e3fbb97e352e34030050e15f67fe0
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-21 14:58:05 -0800

    added user reference to skills model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_211934) do
+ActiveRecord::Schema.define(version: 2020_01_21_225726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2020_01_21_211934) do
     t.integer "order"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +79,5 @@ ActiveRecord::Schema.define(version: 2020_01_21_211934) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "skills", "users"
 end
