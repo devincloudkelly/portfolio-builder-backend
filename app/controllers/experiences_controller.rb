9ca86408a3a538d283f1d587e03ba0161f439b7b
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-21 17:43:29 -0800

    built out experience controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,5 +1,10 @@
 class ExperiencesController < ApplicationController
 
+    def index
+        experiences = Experience.all
+        render json: experiences
+    end
+    
     def show
         experience = Experience.find_by(id: params[:id])
         render json: experience
@@ -11,20 +16,18 @@ class ExperiencesController < ApplicationController
     end
 
     def update
-        experience = Experience.find_by(params[:user_id])
+        experience = Experience.find_by(params[:id])
         experience_params.update(experience_params)
         render json: experience
     end
 
     def destroy
-
+        experience = Experience.find_by(params[:id])
+        experience.destroy
     end
 
     private
 
-    # def experience_params
-    #     params.require(:experience).permit(:company params[:company], :role params[:role], :summary params[:summary], :start_date params[:start_date], :end_date params[:end_date], :external_link params[:external_link], :order params[:order], :user_id params[:user_id])
-    # end
     def experience_params
         params.require(:experience).permit(:company, :role, :summary, :start_date, :end_date, :external_link, :order, :user_id)
     end
