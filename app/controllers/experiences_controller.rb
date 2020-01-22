f8e145ad2a502c25eda8f3cf1f2069339a248c4b
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-21 17:27:46 -0800

    fixed random number issue in seeds

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,32 @@
 class ExperiencesController < ApplicationController
+
+    def show
+        experience = Experience.find_by(id: params[:id])
+        render json: experience
+    end
+
+    def create
+        experience = Experience.create(experience_params)
+        render json: experience
+    end
+
+    def update
+        experience = Experience.find_by(params[:user_id])
+        experience_params.update(experience_params)
+        render json: experience
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    # def experience_params
+    #     params.require(:experience).permit(:company params[:company], :role params[:role], :summary params[:summary], :start_date params[:start_date], :end_date params[:end_date], :external_link params[:external_link], :order params[:order], :user_id params[:user_id])
+    # end
+    def experience_params
+        params.require(:experience).permit(:company, :role, :summary, :start_date, :end_date, :external_link, :order, :user_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # resources :accolades
-  # resources :experiences
-  # resources :educations
-  # resources :projects
-  # resources :skills
+  resources :accolades
+  resources :experiences
+  resources :educations
+  resources :projects
+  resources :skills
   resources :users
   # get '/edit/:user_id', to: 'users#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
