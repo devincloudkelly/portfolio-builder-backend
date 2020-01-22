a98e43f7071c535c2ae85a7bb2e4c5478d330e43
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-22 09:06:43 -0800

    controller actions complete for all controllers

--- app/controllers/accolades_controller.rb
+++ app/controllers/accolades_controller.rb
@@ -1,2 +1,33 @@
 class AccoladesController < ApplicationController
+    def index
+        accolades = Accolade.all
+        render json: accolades
+    end
+    
+    def show
+        accolade = Accolade.find_by(id: params[:id])
+        render json: accolade
+    end
+
+    def create
+        accolade = Accolade.create(accolade_params)
+        render json: accolade
+    end
+
+    def update
+        accolade = Accolade.find_by(id: params[:id])
+        accolade_params.update(accolade_params)
+        render json: accolade
+    end
+
+    def destroy
+        accolade = Accolade.find_by(id: params[:id])
+        accolade.destroy
+    end
+
+    private
+
+    def accolade_params
+        params.require(:accolade).permit(:name, :summary, :link, :order, :user_id)
+    end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,33 @@
 class SkillsController < ApplicationController
+    def index
+        skills = Skill.all
+        render json: skills
+    end
+    
+    def show
+        skill = Skill.find_by(id: params[:id])
+        render json: skill
+    end
+
+    def create
+        skill = Skill.create(skill_params)
+        render json: skill
+    end
+
+    def update
+        skill = Skill.find_by(id: params[:id])
+        skill_params.update(skill_params)
+        render json: skill
+    end
+
+    def destroy
+        skill = Skill.find_by(id: params[:id])
+        skill.destroy
+    end
+
+    private
+
+    def skill_params
+        params.require(:skill).permit(:category, :name, :proficiency, :link_url, :order, :user_id)
+    end
 end
